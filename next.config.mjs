de51886fe2cc85c60e0ed5c764065cf99247487f
mejkyss <124203408+mejkyss@users.noreply.github.com>
2026-01-26 20:01:43 +0000

    fix: correct repo name declaration and adjust image configuration for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const repo = "grafik-made"; // <-- your repo name
+
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
   trailingSlash: true,
+  images: { unoptimized: true },
+
+  // IMPORTANT for GitHub Pages project sites
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
